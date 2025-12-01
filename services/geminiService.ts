65e71ec2ed2e544f5837478b49c877a029111548
kurlovandrej9-del <kurlovandrej9@gmail.com>
2025-12-01 18:53:34 +0200

    аа

--- services/geminiService.ts
+++
@@ -1,6 +0,0 @@
-import { GameState } from "../types";
-
-// Service Disabled for Static Deployment
-export const getOracleAdvice = async (state: GameState): Promise<string> => {
-  return "Сервис временно недоступен.";
-};
